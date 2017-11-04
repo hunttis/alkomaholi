8dc9301ebcff8092b1d6e93b0c7536cf0aa5d0ed
Ville Nuutinen <hunttis@me.com>
2017-11-04 14:43:15 +0200

    Fixed some issues relating to dependencies being split away from the main 'classes'

--- alkoloader.js
+++ alkoloader.js
@@ -2,6 +2,9 @@
 
 const LocalCache = require('./localcache');
 const localCache = new LocalCache();
+const fetch = require('node-fetch');
+const moment = require('moment');
+var xlsx = require('xlsx');
 
 const urlStart = "https://www.alko.fi/INTERSHOP/static/WFS/Alko-OnlineShop-Site/-/Alko-OnlineShop/fi_FI/Alkon%20Hinnasto%20Tekstitiedostona/";
 const filenameStart = "alkon-hinnasto-tekstitiedostona";
@@ -27,7 +30,7 @@ class AlkoLoader {
       return cachedData;
     } else {
       console.log('No cached data, retrieving from Alko');
-      return retrieveData(forDate);
+      return this.retrieveData(forDate);
     }
   }
 
@@ -36,7 +39,7 @@ class AlkoLoader {
     console.log("Loading data for: " + dateString);
     
     var fullUrl = urlStart + filenameStart + dateString + fileExtension;
-    console.log("No data yet, loading from: ", fullUrl);
+    console.log("Loading from URL: ", fullUrl);
     
     fetch(fullUrl)
     .then(response => response.buffer())
@@ -48,15 +51,15 @@ class AlkoLoader {
       var sheet = xlsx.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]], 
         {header: alkoHeaders});
         localCache.setItem("alkodata" + dateString, JSON.stringify(sheet));
-      loadedData = sheet;
       console.log("--- End of data");
       // res.send("server ready");
       // res.send(JSON.stringify(loadedData));
     })
     .catch((error) => {
+      console.log(error);
       console.log("No data for this day on alko's servers!");
-      var dayBefore = moment().subtract(1, 'day');
-      return getDataForSpecificDay(dayBefore);
+      var dayBefore = forDate.subtract(1, 'day');
+      return this.getDataForSpecificDay(dayBefore);
     });
   }
 
