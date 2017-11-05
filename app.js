4b394bca5517ade523dca302ae81312b7fddb4db
Antti Kontoniemi <antti.kontoniemi@wunderdog.fi>
2017-11-05 11:55:29 +0200

    Use let and const instead of var. Also remove some unused variables.

--- app.js
+++ app.js
@@ -1,43 +1,38 @@
 'use strict';
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var http = require('http');
-var xlsx = require('xlsx');
-var cors = require('cors');
-var moment = require('moment');
-var app = express();
-var fetch = require('node-fetch');
-var fileType = require('file-type');
-var Bluebird = require('bluebird');
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const moment = require('moment');
+const fetch = require('node-fetch');
+const Bluebird = require('bluebird');
 
-var AlkoLoader = require('./alkoloader');
-var alkoLoader = new AlkoLoader();
+const AlkoLoader = require('./alkoloader');
+const alkoLoader = new AlkoLoader();
 console.log(alkoLoader);
 
 fetch.Promise = Bluebird;
 
-var app = express();
+const app = express();
 app.use(cors());
 
-var localStorage;
-var loadedData;
+let localStorage;
+let loadedData;
 
 if (typeof localStorage === "undefined" || localStorage === null) {
   console.log("Create local storage");
-  var LocalStorage = require('node-localstorage').LocalStorage;
+  const LocalStorage = require('node-localstorage').LocalStorage;
   localStorage = new LocalStorage('./alko', 20 * 1024 * 1024);
   console.log("Created local storage");
 }
 
 // var loadedData = JSON.parse(localStorage.getItem("alkodata" + dateString));
-var server = http.createServer(app);
+const server = http.createServer(app);
 
 initializeServer();
 
 function initializeServer() {
-  var today = moment();
+  const today = moment();
   loadedData = alkoLoader.getDataForSpecificDay(today);
 }
 
@@ -67,15 +62,15 @@ app.get('/refreshdata', function(req, res, next) {
 
 app.use(express.static('public'));
 
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 app.set('port', port);
 
 server.listen(port);
 server.on('listening', onListening);
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
+  const addr = server.address();
+  const bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
   console.log('Listening on ' + bind);
